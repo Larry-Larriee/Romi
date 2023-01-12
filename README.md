e2b39b6a6b9da20ce31b11fcb3f55e1e403f9eb0
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-01-11 18:21:58 -0800

    PID SETUP

---
+++ README.md
@@ -0,0 +1,15 @@
+Encoders help us figure out how far our robot has traveled (ex. autonomous mode)
+Gyroscopes help us figure out the orientation
+
+Drivetrain subsystem is the robot's drivebase which is what the robot uses to go forwards and backwards
+Roborio bluetooth is the motherboard of the robot sends the code to everything in the robot
+
+Elevator subsystem is the robot's elevator which is what the robot uses to go up and down (hook climber & ball loader)
+Turret subsystem is the shooter
+Limelight is using a camera to detect where we should be placing balls or cubes or whatever
+
+
+https://drive.google.com/file/d/1Si0Rfj8Z7dkYm7ydfaXOw0-R2a0vZEjC/view --> Romi HW
+https://drive.google.com/file/d/1kvSHSnxYRC0fbR3kCaNXT57Kra7vGNyG/view --> Explaining Romi Commands and RobotContainer
+
+https://docs.google.com/document/d/1CuIr3kjJXj0_wxHY0MBgNaQVEkBGwYig1TXS5oDpMHU/edit --> HW #8
---
+++ src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.math.controller.PIDController;
+import frc.robot.sensor.RomiGyro;
+
+public class RomiDrivetrain extends SubsystemBase {
+  private static final double kCountsPerRevolution = 1440.0;
+  private static final double kWheelDiameterInch = 2.75591; // 70 mm
+
+  // The Romi has the left and right motors set to
+  // PWM channels 0 and 1 respectively
+  private final Spark m_leftMotor = new Spark(0);
+  private final Spark m_rightMotor = new Spark(1);
+
+  // The Romi has onboard encoders that are hardcoded
+  // to use DIO pins 4/5 and 6/7 for the left and right
+  private final Encoder m_leftEncoder = new Encoder(4, 5);
+  private final Encoder m_rightEncoder = new Encoder(6, 7);
+
+  // Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  // Set up the PID controller and Gyro
+  public PIDController m_pidController = new PIDController(0.1, 0, 0);
+  public RomiGyro m_gyro = new RomiGyro();
+
+  /** Creates a new RomiDrivetrain. DEFAULT CODE */
+  public RomiDrivetrain() {
+    // Use inches as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    resetEncoders();
+
+    // Invert right side since motor is flipped
+    m_rightMotor.setInverted(true);
+  }
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, straightPIDLine());
+  }
+
+  // Gyro has a method called getAngle and pidController calculates how much you need to turn
+  // to return to angle 0 using the gyro angle data type
+  public double straightPIDLine(){
+    double calculate = m_pidController.calculate(m_gyro.getAngle(), 0); // calculate the angle
+
+    return calculate;
+  }
+  
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+
+  public double getLeftDistanceInch() {
+    return m_leftEncoder.getDistance();
+  }
+
+  public double getRightDistanceInch() {
+    return m_rightEncoder.getDistance();
+  }
+
+  // get the average because encoders may not be super accurate
+  public double getAverageDistanceInch(){ 
+    return (getLeftDistanceInch() + getRightDistanceInch()) / 2; 
+  }
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
